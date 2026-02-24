4e25bff1c15d454a0faf6b2e8b2c145d6aaef1b5
JRV-XVI <jrv.software@outlook.com>
2026-02-24 00:08:54 -0600

    Fixed InnerSchoolSystem.java

--- InnerSchoolSystem.java
+++ InnerSchoolSystem.java
@@ -7,20 +7,30 @@ import java.util.List;
 public class InnerSchoolSystem {
 	private List<String> students = new ArrayList<>();
 
-	InnerSchoolSystem(ArrayList<String> students) {
-		this.students = students;
+	InnerSchoolSystem() {
+		System.out.println("New system");
 	}
 
 	public void addStudent(String name) {
 		students.add(name);
 	}
 
+	public List<String> getList() {
+		return students;
+	}
+
+	public String getStudent(int index) {
+		return students.get(index);
+	}
+
 	public static void main(String[] args) {
-		ArrayList<String> initialArrayList = new ArrayList<>();
-		initialArrayList.add("Joe");
-		InnerSchoolSystem schoolSystem = new InnerSchoolSystem(initialArrayList);
-		schoolSystem.addStudent("Halo");
-		schoolSystem.addStudent("Damm");
-		System.out.println(initialArrayList);
+		InnerSchoolSystem schoolSystem = new InnerSchoolSystem();
+		System.out.println(schoolSystem.getList());
+		schoolSystem.addStudent("John");
+		System.out.println(schoolSystem.getList());
+		schoolSystem.addStudent("Martin");
+		System.out.println(schoolSystem.getList());
+		System.out.println("Consulting student at index 0");
+		System.out.println(schoolSystem.getStudent(0));
 	}
 }
